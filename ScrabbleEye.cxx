a60e9b09707ef6e0b07745067bb744e55dcabf4d
Jacek Królikowski <nietaki@gmail.com>
2012-08-06 23:36:25 +0200

    ScrabbleEye.cxx reformatted

--- ScrabbleEye.cxx
+++ ScrabbleEye.cxx
@@ -2,33 +2,46 @@
 #include "opencv2/imgproc/imgproc.hpp"
 
 #include <iostream>
+#include <vector>
 
 using namespace cv;
 using namespace std;
 
 void help()
 {
- cout << "\nThis program demonstrates line finding with the Hough transform.\n"
-         "Usage:\n"
-         "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
+  cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+          "Usage:\n"
+          "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
 }
 
 int main(int argc, char** argv)
 {
- const char* filename = argc >= 2 ? argv[1] : "res/bed/bed_board.jpg";
-
- Mat src = imread(filename, 0);
- if(src.empty())
- {
-     help();
-     cout << "can not open " << filename << endl;
-     return -1;
- }
-
- Mat dst, cdst;
- Canny(src, dst, 50, 200, 3);
- cvtColor(dst, cdst, CV_GRAY2BGR);
-
+  const char* filename = argc >= 2 ? argv[1] : "res/bed/bed_board.jpg";
+ 
+  Mat src_bgr = imread(filename);
+ 
+  if(src_bgr.empty())
+  {
+    help();
+    cout << "can not open " << filename << endl;
+    return -1;
+  }
+  CV_Assert(src_bgr.depth() == CV_8U);
+  CV_Assert(src_bgr.channels() == 3);
+  Mat src_hsv;
+  vector<Mat> bgr_channels, hsv_channels;
+  cvtColor(src_bgr, src_hsv, CV_BGR2HSV);
+  
+  split(src_hsv, hsv_channels);
+  split(src_bgr, bgr_channels);
+  Mat src = bgr_channels[2];
+  
+ 
+ 
+  Mat dst, cdst;
+  Canny(src, dst, 50, 200, 3);
+  cvtColor(dst, cdst, CV_GRAY2BGR);
+ 
   vector<Vec4i> lines;
   HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
   for( size_t i = 0; i < lines.size(); i++ )
@@ -36,11 +49,11 @@ int main(int argc, char** argv)
     Vec4i l = lines[i];
     line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
   }
-
- imshow("source", src);
- imshow("detected lines", cdst);
-
- waitKey();
-
- return 0;
+ 
+  imshow("source", src);
+  imshow("detected lines", cdst);
+ 
+  waitKey();
+ 
+  return 0;
 }
