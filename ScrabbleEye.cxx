9e502eee54c5024a44739b87923418ba22b20122
Jacek Królikowski <nietaki@gmail.com>
2012-08-06 20:52:32 +0200

    first line recognition demo

--- ScrabbleEye.cxx
+++ ScrabbleEye.cxx
@@ -1,19 +1,46 @@
-#include <cv.h>
-#include <highgui.h>
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
 
-int main ( int argc, char **argv )
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void help()
 {
-  cvNamedWindow( "My Window", 1 );
-  IplImage *img = cvCreateImage( cvSize( 640, 480 ), IPL_DEPTH_8U, 1 );
-  CvFont font;
-  double hScale = 1.0;
-  double vScale = 1.0;
-  int lineWidth = 1;
-  cvInitFont( &font, CV_FONT_HERSHEY_SIMPLEX | CV_FONT_ITALIC,
-              hScale, vScale, 0, lineWidth );
-  cvPutText( img, "Hello World!", cvPoint( 200, 400 ), &font,
-             cvScalar( 255, 255, 0 ) );
-  cvShowImage( "My Window", img );
-  cvWaitKey();
-  return 0;
+ cout << "\nThis program demonstrates line finding with the Hough transform.\n"
+         "Usage:\n"
+         "./houghlines <image_name>, Default is pic1.jpg\n" << endl;
+}
+
+int main(int argc, char** argv)
+{
+ const char* filename = argc >= 2 ? argv[1] : "res/bed/bed_board.jpg";
+
+ Mat src = imread(filename, 0);
+ if(src.empty())
+ {
+     help();
+     cout << "can not open " << filename << endl;
+     return -1;
+ }
+
+ Mat dst, cdst;
+ Canny(src, dst, 50, 200, 3);
+ cvtColor(dst, cdst, CV_GRAY2BGR);
+
+  vector<Vec4i> lines;
+  HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
+  for( size_t i = 0; i < lines.size(); i++ )
+  {
+    Vec4i l = lines[i];
+    line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+  }
+
+ imshow("source", src);
+ imshow("detected lines", cdst);
+
+ waitKey();
+
+ return 0;
 }
